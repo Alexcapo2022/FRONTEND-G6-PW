22c0d33484284525922d7a2c754f14b46f9b4bbf
Alexander Cruz <20192659@aloe.ulima.edu.pe>
2022-11-09 15:34:52 -0500

    Se agrega About

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import Login from './pages/Login'
 import NavBar from './components/NavBar';
 import NotFoundPage from './pages/NotFoundPage';
 import Reviews from './pages/Reviews';
+import Registro from './pages/Registro';
+import Footer from './components/Footer';
+import About from './pages/About';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -12,13 +15,17 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NavBar/>
+    
   <BrowserRouter>
   <Routes>
     <Route path='/PaginaPrincipal' element={<PaginaPrincipal />}/>
     <Route path='/' element={<Login />}/>
+    <Route path='/Registro' element={<Registro />}/>
+    <Route path='/About' element={<About />}/>
     <Route path='*' element={<NotFoundPage />}/>
     <Route path='/Reviews' element={<Reviews/>}/>
   </Routes>
   </BrowserRouter>
+  <Footer/>
   </React.StrictMode>
 );
